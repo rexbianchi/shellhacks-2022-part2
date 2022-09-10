4631b323652b59b81806786b2002501017bb5ba9
Rex Bianchi <rexbianchi@protonmail.com>
2022-09-10 16:42:09 -0400

    Data.py rough draft

---
+++ data.py
@@ -0,0 +1,46 @@
+from io import StringIO
+from os.path import exists
+import pandas as pd
+
+
+def decrypt():
+    pass
+
+def encrypt():
+    pass
+
+def new_account():
+    pass
+
+class Vault:
+    def __init__(self, encrypted_vault_location: str, vault_name, password):
+        self.vault_location = encrypted_vault_location
+
+    def get_accounts(self):
+        if not exists(self.vault_location):
+            new_account()
+            return {}
+        else:
+            with open(self.vault_location) as file:
+                encrypted_text = file.read()
+
+            csv_string = """myucf,rexb,08182003\nmybank,rexbianchi,ssfri48\nanotherbank,fdjhdkdd,dfjdhjdhjd"""
+
+            csv_stringio = StringIO(decrypt(encrypted_text))
+            columns = ["Account", "Username", "Password"]
+            df = pd.read_csv(csv_stringio, sep=",", header=None, names=columns, index_col="Account")
+
+            dictionary = df.to_dict(orient="index")
+            return dictionary
+
+    def save_accounts(self, accounts: dict):
+        df = pd.DataFrame.from_dict(accounts, orient="index")
+        csv_stringio = StringIO()
+        df.to_csv(csv_stringio, sep=",", header=False, index=False)
+        encrypted_string = encrypt(csv_stringio.getvalue())
+        with open(self.vault_location, "w") as file:
+            file.write(encrypted_string)
+
+if __name__ == "__main__":
+    Vault("lol").save_accounts({'myucf': {'Username': 'rexb', 'Password': '08182003'}, 'mybank': {'Username': 'rexbianchi', 'Password': 'ssfri48'}, 'anotherbank': {'Username': 'fdjhdkdd', 'Password': 'dfjdhjdhjd'}}
+)
--- gui/gui.py
+++ gui/gui.py
@@ -2,7 +2,20 @@ import tkinter as tk
 
 
 class App(tk.Tk):
-    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames
+
+
+
 
 
 
