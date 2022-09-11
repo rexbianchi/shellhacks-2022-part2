de53bcd09c64294f27ce0b79c76e206599173e05
Rex Bianchi <rexbianchi@protonmail.com>
2022-09-11 00:31:12 -0400

    NoAccountMade page made

--- gui/gui.py
+++ gui/gui.py
@@ -4,16 +4,15 @@ import tkinter as tk
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        self.geometry("720x480")
 
         container = tk.Frame(self)
+        container.config(padx=190, pady=50)
         container.pack(side="top", fill="both", expand=True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (StartPage, Page1):
+        for F in (NoAccountPage, HasAccountPage):
 
             frame = F(container, self)
 
@@ -21,30 +20,60 @@ class App(tk.Tk):
 
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(NoAccountPage)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 
-class StartPage(tk.Frame):
+class Page(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        # label = tk.Label(self, text="Lonk is Currently Lonked", font=("Times New Roman", 40, "bold"))
-        # label.grid(row=0, column=0)
+        # Main label and canvas
+        self.label = tk.Label(self, font=("Times New Roman", 14, "bold"), pady=20)
+        self.logo_canvas = tk.Canvas(self, height=99, width=320, bg="black")
 
-        self.lonk_logo = tk.PhotoImage(file=r"C:\Users\rexbi\Desktop\shellhacks-2022-part2\gui\assets\LONK-closed.png")
-        canvas = tk.Canvas(self, width=1000, height=1000)
-        canvas.create_image(500, 500, image=self.lonk_logo)
-        # canvas.create_text(75, 100, text="Yo", font=("Times New Roman", 40, "bold"))
-        canvas.grid(row=0, column=0)
+        # This stores the vault password label and input
+        self.vault_password_label = tk.Label(self, font=("Times New Roman", 14, "bold"), pady=20)
+        self.vault_password_input = tk.Entry(self, width=50)
 
 
-class Page1(tk.Frame):
+        # This will get plaintext account info and make and store labels
+        self.vault_information = []
+
+        # THis is to add new password
+        self.account_label = tk.Label(self, text="Account", font=("Times New Roman", 14, "bold"))
+        self.account_entry = tk.Entry(self, width=10)
+        self.username_label = tk.Label(self, text="Username", font=("Times New Roman", 14, "bold"))
+        self.username_entry = tk.Entry(self, width=10)
+        self.password_label = tk.Label(self, text="Password", font=("Times New Roman", 14, "bold"))
+        self.password_entry = tk.Entry(self, width=10)
+
+        self.submit_button = tk.Button(self, text="Submit", width=12)
+
+
+class NoAccountPage(Page):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        super().__init__(parent, controller)
+
+        self.label.config(text="Looks like you're not lonked yet!")
+        self.label.grid(row=0, column=0, columnspan=2, pady=10)
+
+        self.lonk_logo = tk.PhotoImage(file="assets/low-res/LONK-open.png")
+        self.lonk_logo_img = self.logo_canvas.create_image(160, 50, image=self.lonk_logo)
+        self.logo_canvas.grid(row=1, column=0, columnspan=2, pady=10)
+
+        self.password_label.config(text="Choose vault password:")
+        self.password_label.grid(row=2, column=0, pady=10)
+        self.password_entry.grid(row=2, column=1, pady=10)
+        self.submit_button.grid(row=3, column=0, columnspan=3)
+
+
+class HasAccountPage(Page):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
 
 
 if __name__ == "__main__":
